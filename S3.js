aba4a28c3855f53cbfdd9c9996487c8368f397f2
Michael Antoniades <mantoniades@terillium.com>
2023-02-20 16:40:43 -0500

    added v1 for s3 folder creation and detection

--- S3.js
+++ S3.js
@@ -3,6 +3,11 @@ const multerS3 = require('multer-s3');
 const AWS = require('aws-sdk');
 
 
+//To-Do create a function and export for Data being added to folders,
+// example data transmission: https://github.com/bobboy65/Beat-Website-Production-Build/blob/main/src/pages/Upload.js
+
+const S3_Folders = async(email) => {
+
 const ID = process.env.AWS_ACCESS_KEY_ID;
 const SECRET = process.env.AWS_SECRET_ACCESS_KEY;
 const BUCKET_NAME_3 = process.env.AWS_BUCKET_NAME_3;
@@ -30,27 +35,39 @@ var s3 = new AWS.S3({
 //         },
 //     })
 // })
+var user = JSON.parse(JSON.stringify("potato_pancake"))
 
-////////////////////////////////////////////////////////////////////////////////////////
+// function to create a new folder in S3 bucket
 
-function checkFolderExists(email) {
-    const params = {
-      Bucket: 'your-bucket-name',
-      Prefix: email + '/'
-    };
-};
 
+function createFolder(email) {
+  const params = {
+    Bucket: BUCKET_NAME_3,
+    Key: email + '/',
+    Body: '',
+    ACL: 'public-read'
+  };
+
+  s3.upload(params, function(err, data) {
+    if (err) {
+      console.log('Error creating folder: ', err);
+    } else {
+      console.log('Folder created successfully');
+    }
+  });
+}
+}
 //app.post('/upload', upload.array('fileUpload', 2), async (req, res) => {    
 
-    var user = JSON.parse(JSON.stringify("potato_pancake"))
+    
 //console.log(req.user);
 //console.log(req.files);
 //console.log(keyGen)     THIS SECTION POPULATES UPLOAD INFORMATION
-const params = {
-    Bucket: BUCKET_NAME_3,
-    Key: "ART_ANTIGEN", 
-    user: user,
-}
+// const params = {
+//     Bucket: BUCKET_NAME_3,
+//     Key: "ART_ANTIGEN", 
+//     user: user,
+// }
 
 
 // s3.upload(params, function(err) {
@@ -66,23 +83,7 @@ const params = {
 //app.use("/dB" , authMongoDb)
 
 
-// // function to create a new folder in S3 bucket
-// function createFolder(email) {
-//   const params = {
-//     Bucket: 'your-bucket-name',
-//     Key: email + '/',
-//     Body: '',
-//     ACL: 'public-read'
-//   };
 
-//   s3.upload(params, function(err, data) {
-//     if (err) {
-//       console.log('Error creating folder: ', err);
-//     } else {
-//       console.log('Folder created successfully');
-//     }
-//   });
-// }
 
 // // function to check if folder already exists
 // function checkFolderExists(email) {
@@ -107,3 +108,29 @@ const params = {
 // // example usage
 // const userEmail = 'example@example.com';
 // checkFolderExists(userEmail);
+
+
+
+
+// In Node.js, router is a mechanism to define routes and their corresponding handlers, which allow an application to respond to specific requests. app.use is a method that registers middleware functions with the application's routing system. Middleware functions are functions that have access to the request and response objects, and the next middleware function in the application's request-response cycle.
+
+// In the given code snippet, const userControl = require("./routes/user") is importing a module named "user" located in the "routes" directory of the application. This module is expected to define a set of routes for handling user-related requests.
+
+// The line app.use("/user", userControl) registers the userControl module as a middleware function for the application, and maps it to the path "/user". This means that any requests to paths that start with "/user" will be handled by the routes defined in the userControl module.
+
+// For example, if the application receives a request to the path "/user/login", the userControl module will handle it according to the route defined for that path. Similarly, if the application receives a request to the path "/user/profile", the userControl module will handle it according to the route defined for that path.
+
+// In summary, the code is defining and registering a set of routes for user-related requests using the userControl module and mapping it to the "/user" path.
+
+
+//function export
+// vs router export (uses paths in router)
+// module.exports = router;
+// module.exports = {
+//     tokenVerify
+// }
+
+
+module.exports = {
+    S3_Folders
+}
